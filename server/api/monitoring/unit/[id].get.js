921dc40b3634e8b989920ad7802992c9f2507871
alannuarii <alannuariii@gmail.com>
2026-09-09 15:01:56 +0800

    feat: implement prioritized multi-source frequency polling for units 6 and 7 and add diagnostic influx test scripts

--- server/api/monitoring/unit/[id].get.js
+++ server/api/monitoring/unit/[id].get.js
@@ -30,20 +30,40 @@ export default defineEventHandler(async (event) => {
         const pmQuery = buildDGQuery(config.influxBucket, measurement)
         let pmResult = await queryInfluxDB(config, pmQuery)
         
-        // Use Grid/System Frequency from DSE 8610 MKII (PM-DG) for Units 6 and 7
+        // Use Grid/System Frequency for Units 6 and 7
         if (id === '6' || id === '7') {
             try {
-                const syncMeasurement = `PM-DG${id}`
-                const syncQuery = buildDGQuery(config.influxBucket, syncMeasurement)
+                // Query multiple PMs to get the most precise system frequency (like index.vue does)
+                const syncQuery = `
+                    from(bucket: "${config.influxBucket}")
+                      |> range(start: -1m)
+                      |> filter(fn: (r) => r._measurement == "PM-DG9" or r._measurement == "PM-DG8" or r._measurement == "PM-DG7" or r._measurement == "PM-DG6" or r._measurement == "PM-DG1")
+                      |> filter(fn: (r) => r._field == "Frequency")
+                      |> last()
+                `
                 const syncResult = await queryInfluxDB(config, syncQuery)
                 
-                const syncFreq = syncResult.find(item => item._field === 'Frequency')
-                if (syncFreq) {
+                // Prioritize PM-DG9 and PM-DG8 as they have higher precision (PM 5350)
+                const measurements = ['PM-DG9', 'PM-DG8', 'PM-DG7', 'PM-DG6', 'PM-DG1']
+                let bestFreq = null
+                for (const meas of measurements) {
+                    const found = syncResult.find(item => item._measurement === meas && item._value > 0)
+                    if (found) {
+                        bestFreq = found
+                        break
+                    }
+                }
+
+                if (bestFreq) {
                     const freqIndex = pmResult.findIndex(item => item._field === 'Frequency')
                     if (freqIndex !== -1) {
-                        pmResult[freqIndex]._value = syncFreq._value
+                        pmResult[freqIndex]._value = bestFreq._value
                     } else {
-                        pmResult.push(syncFreq)
+                        pmResult.push({
+                            ...bestFreq,
+                            _field: 'Frequency',
+                            _measurement: measurement
+                        })
                     }
                 }
             } catch (syncErr) {
